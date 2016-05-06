12763da32709133591dc7e3fb291ef792d86c6ce
Evan Ross <evanross@Macintosh-76.local>
2016-05-06 14:53:35 -0700

    first push, holla

---
+++ gitSwitcher.js
@@ -0,0 +1,24 @@
+var day = prompt('what day of the week is it?');
+	switch (day){
+		case 'Sunday':
+			console.log('Sunday funday');
+			break;
+		case 'Monday':
+			console.log('Somebody has a case of the Mondaze');
+			break;
+		case 'Tuesday':
+			console.log('Goodbye ruby tuesday');
+			break;
+		case 'Wednesday':
+			console.log('HUMP DAY');
+			break;
+		case 'Thursday':
+			console.log('Emo band name');
+			break;
+		case 'Friday':
+			console.log('Montell Jordan day');
+			break;
+		case 'Saturday':
+			console.log("Never wake up early on saturday, it's bad for you");
+			break;
+	}
